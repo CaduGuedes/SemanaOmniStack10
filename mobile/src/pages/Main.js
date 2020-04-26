eca7199cfcc1d765f8023a753c7f54c853cd8cdb
Cadu Guedes <caduguedesmg@gmail.com>
2020-04-26 14:56:21 -0300

    Buscando as coordenadas do usuário da aplicação

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native'
 import MapView from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
 function Main(){
-  return <MapView style={{flex: 1}} />;
+  const [currentRegion, setCurrentRegion] = useState(null);
+  useEffect(() => {
+    async function loadInitialPosition(){
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted){ //condição que analisa se usuário deu permissão para usar a localização
+        const {coords} = await getCurrentPositionAsync({
+          enableHighAccuracy: false, //não buscará utilizando gps do celular
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        })
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+
+
+  return <MapView initialRegion={currentRegion} style={styles.map} />;
 }
+
+const styles = StyleSheet.create({
+  map: {
+    flex:1,
+  },
+})
+
 
 export default Main;
