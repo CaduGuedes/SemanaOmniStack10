c41be06c190b441db5abb284896680d8322b01c9
Cadu Guedes <caduguedesmg@gmail.com>
2020-04-26 15:16:13 -0300

    Mostrando usuário no Mapa, juntamente com suas informações

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet } from 'react-native'
-import MapView from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native'
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
 function Main(){
@@ -33,13 +33,55 @@ function Main(){
   }
 
 
-  return <MapView initialRegion={currentRegion} style={styles.map} />;
+  return (
+  <MapView initialRegion={currentRegion} style={styles.map}>
+    <Marker coordinate={{ latitude: -17.2222019, longitude: -42.5901277 }}>
+      <Image style={styles.avatar}source={{ uri: 'https://avatars3.githubusercontent.com/u/16464080?s=460&u=5393e0d4639c8bc5062f28679c8ba6ce4069d363&v=4' }} />
+
+      <Callout>
+        <View style={styles.callout}>
+          <Text style={styles.devName}>Cadu Guedes</Text>
+          <Text style={styles.devBio}>BIOBIOBIOBIO</Text>
+          <Text style={styles.devTechs}>ReactJS, ReactNative, Node.js</Text>
+
+        </View>
+      </Callout>
+    </Marker>
+  </MapView> 
+    );
 }
 
 const styles = StyleSheet.create({
   map: {
     flex:1,
   },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFF'
+  },
+
+  callout: {
+    width: 260,
+  },
+
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+
+  devTechs: {
+    marginTop: 5,
+  },
+  
+
 })
 
 
